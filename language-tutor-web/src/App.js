1fbc79767f83d505adac092a383195fe30b2de2c
noenemy <noenemy@gmail.com>
2021-08-17 09:05:07 +0900

    Add router feature

--- language-tutor-web/src/App.js
+++ language-tutor-web/src/App.js
@@ -13,22 +13,45 @@ import {
 function App() {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand href="/">
-            <i fa name="home"></i> 1:1 Language Tutor using AWS AI/ML
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/game">Game</Nav.Link>
-                <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
-                <Nav.Link href="/debug">Debug</Nav.Link>
-                <Nav.Link href="/test">Test</Nav.Link>
-                <Nav.Link href="#home"><FontAwesomeIcon icon={["fal", "coffee"]} /></Nav.Link>
-              </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <Router>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="/">
+              <i fa name="home"></i> 1:1 Language Tutor using AWS AI/ML
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link href="/home">Home</Nav.Link>
+                  <Nav.Link href="/game">Game</Nav.Link>
+                  <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
+                  <Nav.Link href="/debug">Debug</Nav.Link>
+                  <Nav.Link href="/test">Test</Nav.Link>
+                  <Nav.Link href="#home"><FontAwesomeIcon icon={["fal", "coffee"]} /></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/game">
+              <Game />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard />
+            </Route>
+            <Route path="/debug">
+              <Debug />
+            </Route>
+            <Route path="/test">
+              <Test />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
     </header>
   );
 }
@@ -39,21 +62,27 @@ function Home(){
   )
 }
 
-function SignIn(){
+function Game(){
   return(
-      <h1>Sign In</h1>
+      <h1>Game</h1>
   )
 }
 
-function Album(){
+function Leaderboard(){
   return(
-      <h1>Album</h1>
+      <h1>Leaderbord</h1>
   )
 }
 
-function Pricing(){
+function Debug(){
   return(
-      <h1>Pricing</h1>
+      <h1>Debug</h1>
+  )
+}
+
+function Test(){
+  return(
+      <h1>Test</h1>
   )
 }
 
