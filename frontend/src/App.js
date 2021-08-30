983455d61aad0e86ad98c322d72b1df85167b8f4
noenemy <noenemy@gmail.com>
2021-08-30 12:55:42 +0900

    Update learning feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -97,8 +97,18 @@ function Learning(){
       <h1>Learning</h1>
       <ul>
         <li>Hi, my name is Tutty. </li>
-        <li>What language do you want to learn today?</li>
+        <li>What is your name?</li>
+        Name: <input type="text" name="name" />
+        <li>What language do you want to learn?</li>
+        <Button>English</Button> 
+        <Button>Korean</Button> 
+        <Button>Spanish</Button> 
+        <li>What subject do you want to learn today?</li>
+        <Button>Fruits</Button> 
+        <Button>Animals</Button> 
+        <Button>Greetings</Button> 
       </ul>
+      <Button>Meet the tutor!</Button> 
     </div>
   )
 }
