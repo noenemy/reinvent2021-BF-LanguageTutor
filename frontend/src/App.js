a9cde6b5c3f66efa38faa9070305012302b14531
noenemy <noenemy@gmail.com>
2021-09-13 14:20:18 +0900

    Add home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from "react-boots
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from "react";
+import HomeComponent from './components/home/HomeComponent';
 import LearningComponent from './components/learning/LearningComponent';
 import GameComponent from './components/game/GameComponent';
 import LeaderboardComponent from './components/leaderboard/LeaderBoardComponent';
@@ -68,31 +69,7 @@ function App() {
 
 function Home(){
   return(
-    <div>
-      <h1>Welcome to 1:1 Language Tutor using AWS AI/ML!</h1>
-      <p></p>
-      <Button>Get Started!</Button>
-      <h3></h3>
-      <ul>
-        <li>Boost your language learning with AWS AI/ML services.</li>
-        <li>AWS AI / ML services are the perfect technologies to help users read, write, and speak foreign languages.</li>
-      </ul>
-      <p></p>
-      <h3>Architecture Diagram</h3>
-      <ul>
-        <li>picture here.</li>
-      </ul>
-      <p></p>
-      <p></p>
-      <p></p>
-      <h3>Meet the Team</h3>
-      <ul>
-        <li>Iron Man</li>
-        <li>Hulk</li>
-        <li>Thor</li>
-      </ul>
-      <p></p>
-    </div>
+    <HomeComponent />
   )
 }
 
---
+++ frontend/src/components/home/HomeComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+const HomeComponent = () => {
+
+    return(
+        <div>
+            <h1>Welcome to 1:1 Language Tutor using AWS AI/ML!</h1>
+            <p></p>
+            <Button>Get Started!</Button>
+            <h3></h3>
+            <ul>
+            <li>Boost your language learning with AWS AI/ML services.</li>
+            <li>AWS AI / ML services are the perfect technologies to help users read, write, and speak foreign languages.</li>
+            </ul>
+            <p></p>
+            <h3>Architecture Diagram</h3>
+            <ul>
+            <li>picture here.</li>
+            </ul>
+            <p></p>
+            <p></p>
+            <p></p>
+            <h3>Meet the Team</h3>
+            <ul>
+            <li>Iron Man</li>
+            <li>Hulk</li>
+            <li>Thor</li>
+            </ul>
+            <p></p>
+        </div>
+      );
+};
+
+export default HomeComponent;
