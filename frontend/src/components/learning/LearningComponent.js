28a34160d44bc455fd6605a0f19b6d242ef24a8a
noenemy <noenemy@gmail.com>
2021-10-19 21:56:48 +0900

    Update learning component UI

--- frontend/src/components/learning/LearningComponent.js
+++ frontend/src/components/learning/LearningComponent.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Button } from "react-bootstrap";
+import LoadScreen from '../../assets/images/load_screen.png'
 
-const LearningComponent = () => {
+class LearningComponent extends Component {
 
-    return(
-        <div>
-            <h1>Learning</h1>
-            <ul>
-                <li>Hi, my name is Tutty. </li>
-                <li>What is your name?</li>
-                Name: <input type="text" name="name" />
-                <li>What language do you want to learn?</li>
-                <Button>English</Button> 
-                <Button>Korean</Button> 
-                <Button>Spanish</Button> 
-                <li>What subject do you want to learn today?</li>
-                <Button>Fruits</Button> 
-                <Button>Animals</Button> 
-                <Button>Greetings</Button> 
-            </ul>
-            <Button>Meet the tutor!</Button> 
-        </div>
-      );
+    routeChange = () => {
+        // todo
+    }
+
+    render() {
+        return(
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <img className="img-fluid" src={LoadScreen} width="500" height="400"></img>
+                        </div>
+                        <div className="col-6">
+                            <br />
+                            <h2>Hi, my name is Tutty. I'm your language tutor.</h2>
+                            <br /><br />
+    
+                            <h2>What language do you want to learn?</h2>
+                            <Button>English</Button> 
+                            <Button>Korean</Button> 
+                            <Button>Spanish</Button> 
+                            <br /><br />
+                            <h2>What subject do you want to learn today?</h2>
+                            <Button onClick={this.routeChange}>Fruits</Button> 
+                            <Button>Animals</Button> 
+                            <Button>Greetings</Button> 
+                        </div>
+                    </div>
+                    <div className="row"> 
+    
+                    </div>
+                </div>
+    
+            </div>
+          );
+    }
+
 };
 
 export default LearningComponent;
