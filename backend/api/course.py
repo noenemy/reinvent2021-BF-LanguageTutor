ad8a8b74003b9fc6c0d7e9c6d17b75a0083a236b
Sungshik Jou <liks79@gmail.com>
2021-10-26 07:27:21 +0900

    Change persistence layer from AppSync to DDB

--- backend/api/course.py
+++ backend/api/course.py
@@ -15,7 +15,7 @@ from werkzeug.exceptions import BadRequest
 from gql import gql
 from util import get_appsync_secret, get_graphql_client
 from werkzeug.exceptions import BadRequest, InternalServerError, Conflict
-
+from model import CourseModel, course_deserialize, lecture_deserialize, unit_deserialize
 
 course = Blueprint('courses', __name__)
 
@@ -27,82 +27,123 @@ client = get_graphql_client(api_url, api_key)
 @course.route('/', methods=['GET'], strict_slashes=False)
 def list_courses():
 
-    query = gql(
-        """
-        query {
-          listCourses (filter: {type: {contains: "course"}}, limit: 10) {
-            items {
-              id
-              type
-              course_title
-            }
-          }
-        }
-        """
-    )
     try:
-        result = client.execute(query)
-        response = make_response(result, 200)
+        courses = [course_deserialize(course) for course in CourseModel.course_title_index.scan(limit=30)]
+        response = make_response(jsonify({"listCourses": {"items": courses}}), 200)
         return response
 
     except Exception as e:
-        app.logger.error('Retrieve student list failed: {0}'.format(e))
+        app.logger.error('Retrieve course list failed: {0}'.format(e))
         raise InternalServerError('Something went wrong..')
+
+
+    # query = gql(
+    #     """
+    #     query {
+    #       listCourses (filter: {type: {contains: "course"}}, limit: 10) {
+    #         items {
+    #           id
+    #           type
+    #           course_title
+    #         }
+    #       }
+    #     }
+    #     """
+    # )
+    # try:
+    #     result = client.execute(query)
+    #     response = make_response(result, 200)
+    #     return response
+    #
+    # except Exception as e:
+    #     app.logger.error('Retrieve student list failed: {0}'.format(e))
+    #     raise InternalServerError('Something went wrong..')
 
 
 @course.route('/<id>/lectures', methods=['GET'], strict_slashes=False)
 def list_lectures(id):
 
-    app.logger.info(id)
-
-    query = gql(
-        """
-        query {
-                  listCourses (filter: {type: {contains: "lecture"}}, limit: 10) {
-                    items {
-                      id
-                      lecture_order
-                      lecture_title
-                    }
-                  }
-                }
-        """
-    )
     try:
-        result = client.execute(query)
-        response = make_response(result, 200)
+        lectures = [lecture_deserialize(lecture) for lecture in CourseModel.lecture_order_index.scan(CourseModel.course_ref.startswith(id), limit=30)]
+        response = make_response(jsonify({"listCourses": {"items": lectures}}), 200)
         return response
 
     except Exception as e:
-        app.logger.error('Retrieve student list failed: {0}'.format(e))
+        app.logger.error('Retrieve lecture list failed: {0}'.format(e))
         raise InternalServerError('Something went wrong..')
+
+
+    # app.logger.info(id)
+    #
+    # query = gql(
+    #     """
+    #     query {
+    #               listCourses (filter: {type: {contains: "lecture"}}, limit: 10) {
+    #                 items {
+    #                   id
+    #                   lecture_order
+    #                   lecture_title
+    #                 }
+    #               }
+    #             }
+    #     """
+    # )
+    # try:
+    #     result = client.execute(query)
+    #     response = make_response(jsonify({"listCourses": result}), 200)
+    #     return response
+    #
+    # except Exception as e:
+    #     app.logger.error('Retrieve student list failed: {0}'.format(e))
+    #     raise InternalServerError('Something went wrong..')
 
 
 @course.route('/<id>/lectures/<lec_id>/units', methods=['GET'], strict_slashes=False)
 def list_units(id, lec_id):
 
-
-    app.logger.info(id)
-    app.logger.info(lec_id)
-
-    query = gql(
-        """
-        query {
-                  listCourses (filter: {type: {contains: "unit"}}, limit: 100) {
-                    items {
-                      id
-                      unit_order
-                      unit_title
-                    }
-                  }
-                }
-        """
-    )
     try:
-        result = client.execute(query)
-        response = make_response(result, 200)
+        units = [unit_deserialize(unit) for unit in CourseModel.unit_order_index.scan(CourseModel.lecture_ref.startswith(lec_id), limit=30)]
+        response = make_response(jsonify({"listCourses": {"items": units}}), 200)
         return response
 
     except Exception as e:
-        app.logger.error('Retrieve student list failed: {0}'.format(e))
+        app.logger.error('Retrieve unit list failed: {0}'.format(e))
+        raise InternalServerError('Something went wrong..')
+
+    # app.logger.info(id)
+    # app.logger.info(lec_id)
+    #
+    # query = gql(
+    #     """
+    #     query {
+    #               listCourses (filter: {type: {contains: "unit"}}, limit: 100) {
+    #                 items {
+    #                   id
+    #                   unit_order
+    #                   unit_title
+    #                 }
+    #               }
+    #             }
+    #     """
+    # )
+    # try:
+    #     result = client.execute(query)
+    #     response = make_response(result, 200)
+    #     return response
+    #
+    # except Exception as e:
+    #     app.logger.error('Retrieve student list failed: {0}'.format(e))
+    #     raise InternalServerError('Something went wrong..')
+
+
+@course.route('/<id>/lectures/<lec_id>/units/<unit_id>', methods=['GET'], strict_slashes=False)
+def list_steps(id, lec_id, unit_id):
+
+    try:
+        steps = CourseModel.get(unit_id, 'unit')
+        response = make_response(steps.steps, 200)
+        return response
+
+    except Exception as e:
+        app.logger.error('Retrieve unit list failed: {0}'.format(e))
         raise InternalServerError('Something went wrong..')
---
+++ backend/model/__init__.py
@@ -0,0 +1,108 @@
+import uuid
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, MapAttribute
+from pynamodb.indexes import GlobalSecondaryIndex, LocalSecondaryIndex, IncludeProjection, AllProjection
+
+
+class CourseTitleIndex(GlobalSecondaryIndex):
+    """
+    This class represents a local secondary index
+    """
+
+    class Meta:
+        # All attributes are projected
+        read_capacity_units = 10
+        write_capacity_units = 10
+        projection = AllProjection()
+
+    _type = UnicodeAttribute(hash_key=True)
+    course_title = UnicodeAttribute(range_key=True)
+
+
+class LectureIndex(GlobalSecondaryIndex):
+    """
+    This class represents a local secondary index
+    """
+
+    class Meta:
+        # All attributes are projected
+        read_capacity_units = 10
+        write_capacity_units = 10
+        projection = AllProjection()
+
+    _type = UnicodeAttribute(hash_key=True)
+    lecture_order = NumberAttribute(range_key=True)
+
+
+class UnitIndex(GlobalSecondaryIndex):
+    """
+    This class represents a local secondary index
+    """
+
+    class Meta:
+        # All attributes are projected
+        read_capacity_units = 10
+        write_capacity_units = 10
+        projection = AllProjection()
+
+    _type = UnicodeAttribute(hash_key=True)
+    unit_order = NumberAttribute(range_key=True)
+
+
+class CourseModel(Model):
+    """
+    Language Tutor Course Model
+    """
+
+    class Meta:
+        table_name = 'Course'
+        region = 'us-west-2'
+
+    id = UnicodeAttribute(hash_key=True)
+    _type = UnicodeAttribute(range_key=True)
+
+    course_title_index = CourseTitleIndex()
+    course_title = UnicodeAttribute(null=True)
+
+    course_ref = UnicodeAttribute(null=True)
+    lecture_title = UnicodeAttribute(null=True)
+    lecture_order_index = LectureIndex()
+    lecture_order = NumberAttribute(null=True)
+
+    lecture_ref = UnicodeAttribute(null=True)
+    unit_title = UnicodeAttribute(null=True)
+    unit_order_index = UnitIndex()
+    unit_order = NumberAttribute(null=True)
+
+    # step_order = NumberAttribute(null=True)
+    # steps = MapAttribute(default={})
+    steps = UnicodeAttribute(null=True)
+
+
+def course_deserialize(course):
+    course_json = {}
+    course_json['course_title'] = course.course_title
+    course_json['id'] = course.id
+    course_json['type'] = course._type
+    return course_json
+
+
+def lecture_deserialize(lecture):
+    lecture_json = {}
+    lecture_json['lecture_title'] = lecture.lecture_title
+    lecture_json['id'] = lecture.id
+    lecture_json['lecture_order'] = lecture.lecture_order
+    return lecture_json
+
+
+def unit_deserialize(unit):
+    unit_json = {}
+    unit_json['unit_title'] = unit.unit_title
+    unit_json['id'] = unit.id
+    unit_json['unit_order'] = unit.unit_order
+    return unit_json
+
+
+
+if not CourseModel.exists():
+    CourseModel.create_table(read_capacity_units=10, write_capacity_units=10, wait=True)
