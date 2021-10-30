b5ea8923059c1d75715778bb86f9f7e3d7f55e9f
noenemy <noenemy@gmail.com>
2021-10-30 22:44:28 +0900

    Add transcribe type contents

---
+++ frontend/src/components/learning/Transcribe.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Transcribe extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <br />
+                    <h6><i className="fas fa-question-circle"></i> &nbsp; 
+                    Listen and repeat</h6>
+                </div>
+                    <div className="text-center">
+                    <br />
+                    <div className='row text-center'>
+                        <h1 className='text-primary'>안녕하세요.</h1>
+                        <h4 className='text-muted'>[annyeonghaseyo]</h4>
+                        <h4 className='text-muted'>Hello</h4>
+                    </div>
+                    <br /><br />
+                    <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="">
+                        <h1><i className="fas fa-microphone"></i></h1>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Transcribe;
--- frontend/src/components/learning/Whiteboard.js
+++ frontend/src/components/learning/Whiteboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Rekognition from './Rekognition';
 import Textract from './Textract';
 import Vocabulary from './Vocabulary';
+import Transcribe from './Transcribe';
 
 class Whiteboard extends Component {
 
@@ -35,30 +36,6 @@ function Textbook(props){
     return(
         <div>
             <div dangerouslySetInnerHTML={{ __html: RAWHTML.replace(/\n/g, '<br />')}} />
-        </div>
-    )
-}
-
-function Transcribe(){
-    return(
-        <div>
-            <div>
-                <br />
-                <h6><i className="fas fa-question-circle"></i> &nbsp; 
-                Listen and repeat</h6>
-            </div>
-                <div className="text-center">
-                <br />
-                <div className='row text-center'>
-                    <h1 className='text-primary'>안녕하세요.</h1>
-                    <h4 className='text-muted'>[annyeonghaseyo]</h4>
-                    <h4 className='text-muted'>Hello</h4>
-                </div>
-                <br /><br />
-                <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="">
-                    <h1><i className="fas fa-microphone"></i></h1>
-                </button>
-            </div>
         </div>
     )
 }
